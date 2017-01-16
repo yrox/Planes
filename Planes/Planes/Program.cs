5b43f5e09adce9889586858573e6b3de74da6f2b
Olga Yaroshchuk <yrox152@gmail.com>
2017-01-16 03:44:33 +0300

    unnecessary usings removed

--- Planes/Planes/Program.cs
+++ Planes/Planes/Program.cs
@@ -1,26 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Planes
+﻿namespace Planes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var list = new PlaneList();
-            list.Planes.Add(new Plane("111", "sss", 12, 0));
-            list.Planes.Add(new Plane("22", "sss", 24, 100));
-            list.Planes.Add(new Plane("21", "sss", 0, 50));
-            list.GetAlphabetizedPlanes();
-            list.GetAverageDistance();
-            list.GetMaxDistance();
-            list.GetMaxPlacesPlanes();
-            list.GetMinDistance();
-            list.GetPlanesContainingLetter('1');
+
         }
     }
 }
